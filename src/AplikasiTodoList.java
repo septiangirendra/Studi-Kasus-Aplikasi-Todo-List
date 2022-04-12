c1559e9845be1ad5965d3a4883ea78a50b4bf988
septiangirendra <septiangirendra@gmail.com>
2022-04-12 21:56:40 +0700

    first commit

---
+++ src/AplikasiTodoList.java
@@ -0,0 +1,60 @@
+public class AplikasiTodoList {
+
+    /**
+     * Langkah 1. Membuat Model > implementasi dengan array
+     *
+     * @param args
+     */
+
+    // angka 10 adalah batas untuk menambahkan data
+    public static String[] model = new String[10];
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * Menampilkan todo list
+     */
+    public static void showTodoList(){
+
+    }
+
+    /**
+     * Menambah todo ke list
+     */
+    public static void addTodoList(){
+
+    }
+
+    /**
+     * Menghapus todo list
+     */
+    public static void removeTodoList(){
+
+    }
+
+    /**
+     * view show todo list
+     */
+    public static void viewShowTodoList(){
+
+
+    }
+
+    /**
+     * view addTodolist
+     */
+    public static void viewAddTodoList(){
+
+    }
+
+    /**
+     * viewRemoveTodoList
+     */
+    public static void viewRemodeTodoList(){
+
+    }
+
+
+}
